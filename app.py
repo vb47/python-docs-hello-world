2d5c35e69fa3bb2f7a968b4d60ef91c81b7076a8
Vaibhav <vsalur22@gmail.com>
2024-03-23 12:21:03 +0530

    Added about me link.

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "<h2>Navigate to /about to know about me :-)</h2>"
+
+@app.route("/about")
+def about():
+    return "<h1>Hi, I am Vaibhav.</h1><br/><br/><h3>Naam hi kaafi he.</h3>"
